53ee08ea917be4dee68d8d6845e7ffdad902d705
goncaloperalta <goncaloperalta@ua.pt>
2023-06-09 00:18:24 +0100

    Update to run in AppDaemon

--- RPI/python-scripts/gas_price.py
+++ RPI/python-scripts/gas_price.py
@@ -1,38 +1,15 @@
-import paho.mqtt.client as mqtt
-from requests import get
-import urllib3
-import json
-urllib3.disable_warnings()
+import appdaemon.plugins.hass.hassapi as hass
+import datetime
 
-def getGasPrice():
-    urlHomeAssistant = "http://192.168.1.101:8123/"
-    entityID = "input_text.gas_price_eur_kw"
+class gas_price(hass.Hass):
+    def initialize(self):
+        runtime = datetime.time(0, 0, 0)
+        self.run_minutely(self.getGasValue, runtime)
 
-    # Access token created through Home Assistant profile
-    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIxYzljODRhMzI5NjU0MzUwYjc4YWZkMzdlNWRhMTQxMCIsImlhdCI6MTY4MDA4NTAyOSwiZXhwIjoxOTk1NDQ1MDI5fQ.lWxAax5wjkAOMbkuu88iK2zAlo58xDW7AJiLzsWChDU"
-
-    urlHomeAssistant = f'{urlHomeAssistant}api/states/{entityID}'  #"http://url/api/states/input_text.value"
-    headers = {
-        "Authorization": "Bearer " + token,
-        "content-type": "application/json",
-    }
-    response = get(urlHomeAssistant, headers=headers, verify=False)
-    data = json.loads(response.text)
-    return data['state']
-
-v = getGasPrice()
-
-broker_address = "192.168.1.101"
-broker_port = 1883
-username = "rpi"
-password = "rpimqtt"
-
-client = mqtt.Client()
-client.username_pw_set(username, password)
-client.connect(broker_address, broker_port)
-
-topic = "Gas_Price"
-message = f'{getGasPrice()}'
-client.publish(topic, message, retain=True)
-
-client.disconnect()
+    def getGasValue(self, kwargs):
+        entityID = "input_text.value"
+        self.gasInputEntity = self.get_entity(entityID)
+        gasPrice = self.gasInputEntity.get_state
+        
+        self.gasEntity = self.get_entity("sensor.gasprice")
+        self.gasEntity.set_state(state=gasPrice)
